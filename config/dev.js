d8d1eee3e4381d5b59bdb0b331f3fa29b833471f
koppthe <kopptsoft@gmail.com>
2019-01-29 19:59:01 +0800

    chore: init project

---
+++ config/dev.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"'
+  },
+  defineConstants: {
+  },
+  weapp: {
+    module: {
+      postcss: {
+        // 小程序端样式引用本地资源内联
+        url: {
+          enable: true,
+          limit: 102400000000
+        }
+      }
+    }
+  },
+  h5: {
+    devServer: {
+      host: '0.0.0.0',
+      disableHostCheck: true,
+      port: 8080
+    }
+  }
+}
