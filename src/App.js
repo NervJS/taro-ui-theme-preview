e8c810755506b4a182cf3f77b6316bbfcab77f25
koppthe <kopptsoft@gmail.com>
2019-01-30 17:17:31 +0800

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className='demo-frame'>
+          <iframe title='UI' src='/h5/index.html' frameBorder={0}></iframe>
+          <div className='iphone-frame'></div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
