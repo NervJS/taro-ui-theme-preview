cd0e3a06aca96c37eb88d6f47aaa10fbfe4e26a2
koppthe <kopptsoft@gmail.com>
2019-02-01 19:52:22 +0800

    Updates

---
+++ precache-manifest.37a4af7b8a8eb868501f033ff779684b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ea9318b222d559c977b93682ffa53fe",
+    "url": "./static/media/iframe_iphonex.8ea9318b.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0925305519a8c7c12aa3",
+    "url": "./static/js/main.09253055.chunk.js"
+  },
+  {
+    "revision": "e021ecabdf7bd6e8b8ff",
+    "url": "./static/js/1.e021ecab.chunk.js"
+  },
+  {
+    "revision": "0925305519a8c7c12aa3",
+    "url": "./static/css/main.c477e7f4.chunk.css"
+  },
+  {
+    "revision": "be9340ba60e05c5075d4601886b2392f",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7202fab546864684f9c9763e09f17b6.js"
+  "./precache-manifest.37a4af7b8a8eb868501f033ff779684b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
